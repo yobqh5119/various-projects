15436a4956c5988e1643e66c75b2f78b3ba23f02
Yoo Bin <yqh5119.web@gmail.com>
2018-05-01 13:51:49 -0400

    map changes

--- yoons/src/js/app.js
+++ yoons/src/js/app.js
@@ -12,16 +12,16 @@ $(function(){
       $('nav').removeClass('black');
     }
   });
-})
 
-function initMap() {
-  var uluru = {lat: 40.078277, lng: -75.301571};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 19,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
+  function initMap() {
+    var uluru = {lat: 40.078277, lng: -75.301571};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 19,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
+})
